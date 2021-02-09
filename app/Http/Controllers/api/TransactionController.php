69b4ea95c5fbb33e844c4880acdf7b7645b9982e
Edo C. Setiawan <edocsetiawan@gmail.com>
2021-02-09 21:37:23 +0700

    add email

--- app/Http/Controllers/api/TransactionController.php
+++ app/Http/Controllers/api/TransactionController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Transaction;
 use App\Models\Stock;
 use App\Http\Controllers\Api\TraitApi;
+use App\Mail\CarDealerEmail;
+use Illuminate\Support\Facades\Mail;
 
 class TransactionController extends Controller
 {
@@ -19,18 +21,22 @@ class TransactionController extends Controller
 
      public function addNewTransaction(Request $request)
      {
-        $data       = $request->all();  
+         $data       = $request->all();  
+         $user       = Auth::user();   
+         $transaction['buyer_name']          = $request['name'];
+         $transaction['buyer_email']         = $request['email'];
+         $transaction['buyer_phone_number']  = $request['phone_number'];
+         $transaction['car_id']              = $request['car_id'];
+         $transaction['qty']                 = (int)$request['qty'];
+         $transaction['total_price']         = (float)$request['total_price'];
 
-        $transaction['buyer_name']          = $request['name'];
-        $transaction['buyer_email']         = $request['email'];
-        $transaction['buyer_phone_number']  = $request['phone_number'];
-        $transaction['car_id']              = $request['car_id'];
-        $transaction['qty']                 = (int)$request['qty'];
-        $transaction['total_price']         = (float)$request['total_price'];
+         Transaction::create($transaction);
+         Stock::where('id',$transaction['car_id'])->decrement('stock',$transaction['qty']);
 
-        Stock::where('id',$transaction['car_id'])->decrement('stock',$transaction['qty']);
-
-        Transaction::create($transaction);
+         Mail::send('templateEmail', $transaction, function($message) use($transaction,$user) {
+            $message->to($transaction['buyer_email'], $transaction['buyer_name'])->subject('Invoice');
+            $message->from($user['email'],$user['name']);
+         });
 
         return $this->responseApi('ok','','New stock success insert');
      }
